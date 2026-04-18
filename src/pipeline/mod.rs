ebd61f924aa56c0417192a521b654725dbd12c37
Protonomaly <proto@colorful.localdomain>
2026-04-18 09:29:16 +0200

    Add upper/lower to bounds. Add enable to Filter

--- src/pipeline/mod.rs
+++ src/pipeline/mod.rs
@@ -34,17 +34,25 @@ pub struct ShapeWeight<S> {
 pub struct Bounds {
     pub min: f32,
     pub max: f32,
+    pub lower: f32,
+    pub upper: f32,
 }
 
 impl Bounds {
     pub(crate) const fn new_01() -> Self {
-        Self { min: 0., max: 1. }
+        Self {
+            min: 0.,
+            max: 1.,
+            lower: 0.,
+            upper: 1.,
+        }
     }
 }
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 #[repr(C)]
 pub struct Filter {
+    pub enable: bool,
     pub min_cutoff: f32,
     pub beta: f32,
 }
